212bcaa63ccb8247e2e2d5adc06353b447a1b3a3
YassineLengliz <yassinelengliz69@gmail.com>
2025-08-29 03:12:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // 👈 replace with your GitHub repo name
+  base: "/SolarDroneTech/", // 👈 replace with your GitHub repo name
   server: {
     host: "::",
     port: 8080,
